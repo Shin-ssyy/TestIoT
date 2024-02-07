67a0ffd6db6fcbd0987445e23de4cfb3ad0b1bfd
Shin-ssyy <159232456+Shin-ssyy@users.noreply.github.com>
2024-02-07 16:01:59 +0900

    Create ex01.c
    write number and answer

---
+++ 2nd Day/ex01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    int num;
+    printf("hello world\n");
+
+    printf("Write Number : ");
+    scanf("%d", &num);
+    printf("Answer : %d\n", num);
+
+    return 0;
+}
